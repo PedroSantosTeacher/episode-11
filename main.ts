f47695cb3574ecb22ffe8ace749c5c442647f8b2
Pedro Santos <102796228+MickeyMouseHome@users.noreply.github.com>
2026-01-11 15:56:34 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,59 @@
+let dado = 0
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    dado = randint(1, 6)
+    if (dado == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (dado == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (dado == 3) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (dado == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (dado == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
